6908be5ea1d3ca8648631fecfa414c310e44e02c
shubbush <shubton@gmail.com>
2023-10-25 21:10:12 +0200

    add telescope file explorer

--- shell/.config/nvim/lua/keybindings.lua
+++ shell/.config/nvim/lua/keybindings.lua
@@ -30,3 +30,19 @@ vim.keymap.set('v', "<leader>d", "\"_d")
 
 vim.keymap.set('n', "Q", "<nop>")
 
+
+-- Telescope file browser
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>ft",
+  ":Telescope file_browser<CR>",
+  { noremap = true }
+)
+
+-- open file_browser with the path of the current buffer
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>fc",
+  ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+  { noremap = true }
+)
--- shell/.config/nvim/lua/plugins.lua
+++ shell/.config/nvim/lua/plugins.lua
@@ -22,6 +22,10 @@ require("lazy").setup({
 		'nvim-telescope/telescope.nvim', tag = '0.1.4',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+	},
 	-- Git
 	'tpope/vim-fugitive',
 	'lewis6991/gitsigns.nvim',
