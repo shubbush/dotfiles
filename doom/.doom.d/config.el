d3290b907f6b07a722707b032d51006ff97773cb
shubbush <shubton@gmail.com>
2023-11-29 09:15:58 +0100

    emacs: reload file changes

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -129,6 +129,7 @@
 
 
 (setq confirm-kill-emacs nil)
+(global-auto-revert-mode)
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
