72a2c0ca6b26e69883325e3f90013b68dfeef95d
shubbush <shubton@gmail.com>
2026-02-17 12:32:22 +0100

    enable neogit & disable fuggitive

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -1,19 +1,28 @@
 return {
-	{
-		"NeogitOrg/neogit",
-		enabled = false,
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"sindrets/diffview.nvim", -- optional - Diff integration
+  {
+    "NeogitOrg/neogit",
+    enabled = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",  -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
 
-			-- Only one of these is needed.
-			--		"nvim-telescope/telescope.nvim", -- optional
-			--		"ibhagwan/fzf-lua",            -- optional
-			--		"echasnovski/mini.pick",       -- optional
-		},
-		keys = {
-			{ "<leader>gs", "<cmd>Neogit<cr>", desc = "Open Neogit status buffer" },
-		},
-		config = true,
-	},
+      -- Only one of these is needed.
+      --		"nvim-telescope/telescope.nvim", -- optional
+      --		"ibhagwan/fzf-lua",            -- optional
+      --		"echasnovski/mini.pick",       -- optional
+    },
+    config = function()
+      require("neogit").setup({
+        kind = "split",
+        process_spinner = true,
+        integrations = {
+          diffview = true,
+          fzf_lua = true,
+        }
+      })
+    end,
+    keys = {
+      { "<leader>gs", "<cmd>Neogit<cr>", desc = "Open Neogit status buffer" },
+    },
+  },
 }
